ac0d156481687681b470a7c2467e6c94c3b50f53
Haoyue Wang <whymoon132@gmail.com>
2016-09-06 10:55:33 +0800

    rm all

--- src/act/robot/constant/FaceConstant.java
+++
@@ -1,7 +0,0 @@
-package act.robot.constant;
-
-public class FaceConstant {
-	public static final String API_KEY = "5085f72edc50b8179ef180d6fdff4132";
-	public static final String API_SECRET = "cSvQfZ9CaDxqskPM_HAU7xgfzhsP03Ll";
-	public static final String GROUP_NAME = "test_group";
-}
