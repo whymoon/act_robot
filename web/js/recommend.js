733249a9153828e79690d383f6f6a36b61a8ab31
xxsong <songxinxin429@163.com>
2016-09-23 21:58:06 +0800

    recommend fixed

--- web/js/recommend.js
+++ web/js/recommend.js
@@ -1,6 +1,9 @@
 /**
  * Created by my dell on 2016/8/21.
  */
+var jsonArray = new Array();
+var NUM_PRINT_DATA = 6;
+
 $(document).ready(function(){
     hideAll();
     $('#webcam').resize(640, 480);
@@ -18,11 +21,11 @@ $(document).ready(function(){
      $('.photobooth ul').hide();
 
     $('#take-picture').click(function () {
+        $('#webcam').hide();
+        $('#take-picture').hide();
         $('#update').show();
         $('.trigger').click();
-        $('#webcam').hide();
         $('#title').hide();
-        $('#take-picture').hide();
         $('#name').show();
         $('.sk-circle').show();
     });
@@ -37,7 +40,7 @@ $(document).ready(function(){
 function hideAll() {
     $('.sk-circle').hide();
     $('#title').hide();
-    $('#interset').hide();
+    $('#interest').hide();
     $('#name').hide();
     $('#update').hide();
 }
@@ -62,6 +65,7 @@ function uploadImage(file) {
     fd.append('photo', file);
     $.ajax({
         type: 'POST',
+        async : false, //取消异步
         url: '/act_robot/FaceIdentifyServlet',
         dataType:"json",
         data: fd,
@@ -69,40 +73,99 @@ function uploadImage(file) {
         contentType: false
     }).done(function(data) {
         console.log(data);
+
         var str="识别结果:"+ data["user_name"];
         $('#name').text(str).show();
         $('#title').show();
-        recommend(data["user_name"],0);
-        recommend(data["user_birthplace"],1);
-        recommend(data["user_job"],2);
-        recommend(data["user_department"],3);
-        recommend(data["user_major"],4);
         alert(data["user_birthplace"] + " " + data["user_job"] + " " + data["user_department"]+ " " + data["user_major"]);
+        recommend(data["user_name"],false);
+        recommend(data["user_birthplace"],false);
+        recommend(data["user_job"],false);
+        recommend(data["user_department"],false);
+        recommend(data["user_major"],true);
+        // interested_content();
     });
 }
 
-function recommend(recommend_data,num){
-    var code="";
+function recommend(recommend_data,boolean){
     $.post("/act_robot/RingServlet?ring=no&wd="+ recommend_data,
         function(data) {
+            for(var i = 0;i<data.content.length;i++)
+                window.jsonArray.push(data.content[i]);
+            console.log("数组长度"+window.jsonArray.length);
             console.log(data);
-            quickSort_hot(data.content,0,data.content.length-1);
-            $('.sk-circle').hide();
-            code += "<div class='bs-callout bs-callout-primary' style=' margin :0 auto;width: 800px'>";
-            code += "<h4>" + data.content[0].description + "<small>"+data.content[0].hot+"</small></h4>";
-            code += "</div>";
-            if(num == 0)
-                $('#name_interest').html(code).show();
-            else if(num==1)
-                $('#birthplace_interest').html(code).show();
-            else if(num==2)
-                $('#job_interest').html(code).show();
-            else if(num==3)
-                $('#department_interest').html(code).show();
-            else
-                $('#major_interest').html(code).show();
+            // quickSort_hot(data.content,0,data.content.length-1);
+            // $('.sk-circle').hide();
+            // code += "<div class='bs-callout bs-callout-primary' style=' margin :0 auto;width: 800px'>";
+            // code += "<h4>" + data.content[0].description + "<small>"+data.content[0].hot+"</small></h4>";
+            // code += "</div>";
+            // if(num == 0)
+            //     $('#name_interest').html(code).show();
+            // else if(num==1)
+            //     $('#birthplace_interest').html(code).show();
+            // else if(num==2)
+            //     $('#job_interest').html(code).show();
+            // else if(num==3)
+            //     $('#department_interest').html(code).show();
+            // else
+            //     $('#major_interest').html(code).show();
+            if(boolean == true)
+                interested_content();
         });
 }
+function interested_content() {
+    quickSort_hot(window.jsonArray,0,window.jsonArray.length-1);
+    var number;
+    var code = "",relatedContent = "";
+    if(window.jsonArray.length>NUM_PRINT_DATA)
+        number = NUM_PRINT_DATA;
+    else
+        number = window.jsonArray.length;
+    console.log("num的值"+number + " "+window.jsonArray.length);
+    for(var i = 0;i<number;i++){
+        var hotbar = 100;
+        if (jsonArray[i].hot < 100)
+            hotbar = jsonArray[i].hot;
+        if (i % 2 == 0)
+            code += "<div style='height: 140px'><div style='float:left;width:49%'>";
+        else
+            code += "<div style='float:right;width:49%'>";
+        code += "<div class='weibo_line'>";
+        code += "<div class='event_desc'>";
+        code += "<span class='badge badge-warning'>" + (i + 1) + "</span>";
+        code += "<a href='#' class='robot-answer-title'>" + window.jsonArray[i].description + "</a><br/></div>";
+        var j = i+1;
+        relatedContent += j+ ";"+window.jsonArray[i].description + ";";
+        code += "<div style='margin-left: 5px; margin-bottom: 3px'>";
+        code += "<span class='corewd' style='font-weight: bold'>关键词：</span>";
+        code += "<span class = 'robot-answer-corewd'>" + window.jsonArray[i].corewords + "</span><br/>";
+        relatedContent += "关键词有："+ window.jsonArray[i].corewords + ";";
+        if (window.jsonArray[i].participant != "") {
+            code += "<span style='font-weight: bold'>参与者：</span>";
+            code += "<span class='robot-answer-participant'>" + window.jsonArray[i].participant + "</span><br/>";
+            relatedContent += "参与者有："+ window.jsonArray[i].participant + ";";
+        }
+        if (window.jsonArray[i].eventLoc != "其他") {
+            code += "<span style='font-weight: bold'>地点：</span>";
+            code += "<span class='robot-answer-loc'>" + window.jsonArray[i].eventLoc + "</span><br/>";
+            relatedContent += "地点："+ window.jsonArray[i].eventLoc + ";";
+        }
+        code += "<span style='float: left; font-weight: bold'>热度：</span>";
+        code += "<div class='progress' style='float: left; margin-top: 5px; margin-bottom: 0; height: 10px; width: 150px;'>";
+        code += "<div class='progress-bar progress-bar-danger' style='width:" + hotbar + "%' aria-valuenow='" + hotbar + "' aria-valuemin='0' aria-valuemax='100'>";
+        code += "<div class='bar'></div></div></div>";
+        code += "<span style='margin: 0 0 0 0; font-size: 15px; color: #eb192d;'>&nbsp;" + window.jsonArray[i].hot + "</span></div></div>";
+        relatedContent += "今日热度为："+ window.jsonArray[i].hot + ";";
+        if(i%2==0)
+            code +="</div>";
+        else
+            code +="</div></div>";
+    }
+    $('.sk-circle').hide();
+    $('#interest').html(code).show();
+    speakText(relatedContent);
+}
+
 function quickSort_hot(arr, left, right) {
     var i, j, t, pivot;
     if (left >= right) {
@@ -128,4 +191,13 @@ function quickSort_hot(arr, left, right) {
     arr[j].hot = pivot;
     quickSort_hot(arr, left, i - 1);
     quickSort_hot(arr, i + 1, right);
+}
+function speakText(contentText) {
+    $.post("/act_robot/TtsServlet",
+        {
+            text: contentText
+        },
+        function(data){
+            console.log(data);
+        });
 }
